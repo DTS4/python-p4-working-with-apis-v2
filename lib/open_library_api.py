d9c2637b4e942d0662b8cd722aa1e3d10527b029
Keith <keith.githinji@student.moringaschool.com>
2025-01-16 12:26:29 +0300

    Completed the lab

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -4,20 +4,18 @@ import json
 
 class Search:
 
-    def get_search_results(self):
+    def get_search_results_json(self):
         search_term = "the lord of the rings"
 
         search_term_formatted = search_term.replace(" ", "+")
         fields = ["title", "author_name"]
-        # formats the list into a comma separated string
-        # output: "title,author_name"
         fields_formatted = ",".join(fields)
         limit = 1
 
         URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
-
+        print(URL)
         response = requests.get(URL)
-        return response.content
+        return response.json()
 
     def get_search_results_json(self):
         search_term = "the lord of the rings"
@@ -48,8 +46,8 @@ class Search:
 # results = Search().get_search_results()
 # print(results)
 
-# results_json = Search().get_search_results_json()
-# print(json.dumps(results_json, indent=1))
+results_json = Search().get_search_results_json()
+print(json.dumps(results_json, indent=1))
 
 search_term = input("Enter a book title: ")
 result = Search().get_user_search_results(search_term)
